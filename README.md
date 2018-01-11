78c09b103128921560b29f6fa00f4361e0f1eeb4
baiguiren <baiguiren@kollway.com>
2018-01-11 15:11:03 +0800

    1、添加效果图 2、样式更改 3、当前使用的 session 高亮背景 4、添加清除当前域名 cookie 的功能

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
 # chrome-session-manager
 chrome插件，保存当前tab的当前状态的所有cookie，可以保存多个状态，并且可以还原，后续可以恢复到某一个状态。
 
-### 初衷
-为了保存同一个网站的多个 session。
+### 主要用处
+* 保存同一个网站的多个 session, 比如一个管理后台, 需要在不同角色之间切换。
 
-* 一般情况下，session id 都是通过 cookie 保存
-* 这样，我们就可以使用插件保存当前 cookie; 手动清空 cookie，然后再登录，再保存当前 cookie
-* 清空 cookie, 使用插件切回到上一次清空前的状态
+### 原理说明
+* 0. 正常情况下, 浏览器保存 cookie 都是和域名关联的, 在不同的 tab 之间访问同一个域名, cookie 的修改都会互相影响。
+* 1. 一般情况下，session id 都是通过 cookie 保存
+* 2. 这样，我们就可以使用插件保存当前 cookie; 手动清空 cookie，然后再登录，再保存当前 cookie 状态
+* 3. 使用插件清空当前的cookie, 使用之前保存的 cookie, 就可以回到上一个状态
 
-### 问题
-* 写完之后发现框架(laravel)的 session id 是加密的，并且在每次请求之后都会变化，而且上一次的 session id 会失效。所以目前看来是用不上了。
+### 使用说明
+* 0. 点击插件图标, 在弹出的框的输入框输入当前session名称(好比如用某种角色名称、或者用户名), 点击 `save` 按钮
+* 1. 通过上一步, 已经保存了当前的所有 cookie, 并且现在可以在列表看到我们保存的状态名称
+* 2. 我们点击右上角的 "清除当前页面所有cookie" 按钮, 清空 cookie, 注意: 不要点击退出, 否则之前保存的 session(php、java这些的session) 会失效
+* 3. 这时候再刷新, 会提示登陆, 再次登陆(使用不同账号), 并且输入新的名称, 最后保存
+* 4. 这时候列表保存了两个状态, 我们可以通过点击 `use` 来启用不同的状态
+
+### 其他说明
+* 0. 绿色背景表示当前正在使用的
+* 1. `use`: 启用某一个历史状态
+* 2. `update`: 更新当前 tab 的 cookie 到对应的状态中
+* 3. `remove`: 移除某个历史状态
+
+### 效果图
+* ![主界面](https://github.com/eleven26/chrome-session-manager/blob/master/images/main.png)
+* ![session列表](https://github.com/eleven26/chrome-session-manager/blob/master/images/save.png)
+* ![启用状态](https://github.com/eleven26/chrome-session-manager/blob/master/images/use.png)
--- popup.js
+++ popup.js
@@ -1,15 +1,16 @@
 $(function () {
-    let name;
+    let name = '';
     action(loadAll);
 
     let methods = {
         remove: remove,
         save: save,
         use: use,
-        update: update
+        update: update,
+        clear: clear
     };
 
-    ['remove', 'save', 'use', 'update'].map(function (class_name) {
+    ['remove', 'save', 'use', 'update', 'clear'].map(function (class_name) {
         $(document).on('click', '.' + class_name, function () {
             if (class_name === 'save') {
                 name = $(this).siblings('input').val();
@@ -20,7 +21,78 @@ $(function () {
         });
     });
 
+    /**
+     * 清除当前域名当前tab的所有 cookie
+     * @param {object} url
+     * @param {boolean} alert
+     */
+    function clear(url, alert = true) {
+        chrome.cookies.getAll({ domain: url.host }, function (cookies) {
+            cookies.map(function (cookie) {
+                chrome.cookies.remove({url: url.origin, name: cookie.name}, function () {});
+            });
+            if (alert) alertSuccess('清除成功!');
+        });
+    }
+
+    /**
+     * 提示成功消息
+     * @param {string} msg
+     */
+    function alertSuccess(msg) {
+        $('.alert').html(msg).css('opacity', 1).css('color', 'green');
+        setTimeout(function () {
+            $('.alert').css('opacity', 0);
+        }, 1000);
+    }
+
+    /**
+     * 提示失败信息
+     * @param {string} msg
+     */
+    function alertError(msg) {
+        $('.alert').html(msg).css('opacity', 1).css('color', 'red');
+        setTimeout(function () {
+            $('.alert').css('opacity', 0);
+        }, 1000);
+    }
+
+    /**
+     * 使用历史某一个状态
+     * @param {object} url
+     */
     function use(url) {
+        // 先清空原有的 cookie
+        clear(url);
+
+        getCookiesByName(url, function (storage_cookies) {
+            if (storage_cookies[name].hasOwnProperty('list')) {
+                setHighlight(storage_cookies, name);
+                storage_cookies[name]['list'].map(function (cookie) {
+                    // 设置 storage 存储的 cookie 到当前 tab 的域名中
+                    chrome.cookies.set(cookie, function () {});
+                });
+
+                let obj = {};
+                obj[url.host] = storage_cookies;
+                chrome.storage.local.set(obj, function () {
+                    alertSuccess('启用成功!');
+                });
+            }
+        });
+    }
+
+    /**
+     * 根据 url 中的域名获取当前 name 对应的 cookie 对象
+     * @param {object} url
+     * @param {function} callback 获取 cookie 之后的回调
+     * @return {object}
+     *      {
+     *          'list': [cookie, ...]
+     *          'highlight': 1
+     *      }
+     */
+    function getCookiesByName(url, callback) {
         let domain = url.host;
         chrome.storage.local.get(domain, function (storage_cookies) {
             storage_cookies = storage_cookies || {};
@@ -28,18 +100,31 @@ $(function () {
                 storage_cookies = storage_cookies[domain];
             }
             if (storage_cookies.hasOwnProperty(name)) {
-                let cookies = storage_cookies[name];
-                cookies.map(function (cookie) {
-                    // 设置 storage 存储的 cookie 到当前 tab 的域名中
-                    chrome.cookies.set(cookie, function () {
-                        console.log(cookie);
-                    });
-                });
+                callback(storage_cookies);
             }
-        })
+        });
     }
 
+    /**
+     * 设置当前使用的session高亮, 同时取消其他的高亮状态
+     * @param {object} storage_cookies
+     * @param {string} name
+     */
+    function setHighlight(storage_cookies, name) {
+        highlightLine(name);
+        for (let key in storage_cookies) {
+            if (storage_cookies.hasOwnProperty(key)) {
+                storage_cookies[key].highlight = (key === name) ? 1 : 0;
+            }
+        }
+    }
+
+    /**
+     * 移除session
+     * @param {object} url
+     */
     function remove(url) {
+        if (!name) return;
         let domain = url.host;
         chrome.storage.local.get(domain, function (storage_cookies) {
             storage_cookies = storage_cookies || {};
@@ -52,28 +137,60 @@ $(function () {
                 obj[domain] = storage_cookies;
                 chrome.storage.local.set(obj, function () {
                     removeLine(name);
+                    alertSuccess('删除成功!');
                 });
             }
-        })
+        });
     }
 
-    function update(url) {
-        save(url);
+    /**
+     * 获取当前页面的所有cookie，保存到高亮的session中
+     * @param {string} url
+     * @param {function} callback
+     */
+    function update(url, callback) {
+        save(url, function () {
+            alertSuccess('更新成功!');
+        });
     }
 
-    function save(url) {
+    /**
+     * 保存当前页面的所有cookie
+     * @param {object} url
+     * @param {function} callback
+     */
+    function save(url, callback = null) {
+        if (!name) return;
+        let reg = new RegExp('^[\u4e00-\u9fa5_a-zA-Z0-9]+$');
+        if (!name.match(reg)) {
+            alertError('非法名称!');
+            return ;
+        }
+
         let domain = url.host;
         new Promise(function (resolve, reject) {
             chrome.storage.local.get(domain, function (storage_cookies) {
                 storage_cookies = storage_cookies || {};
                 if (storage_cookies.hasOwnProperty(domain)) {
+                    /**
+                     * {
+                     *  'key1': {
+                               list: [],
+                               highlight: 1
+                     *      }
+                     * }
+                     */
                     storage_cookies = storage_cookies[domain];
                 }
 
+                let cookie_obj = {
+                    list: [],
+                    highlight: 1,
+                };
+                // 获取当前 tab 的所有 cookie
                 chrome.cookies.getAll({ domain: domain }, function (cookies) {
-                    let new_cookies = [];
                     cookies.map(function (cookie) {
-                        new_cookies.push({
+                        cookie_obj.list.push({
                             url: url.origin,
                             name: cookie.name,
                             value: cookie.value,
@@ -83,39 +200,57 @@ $(function () {
                             httpOnly: cookie.httpOnly,
                             expirationDate: cookie.expirationDate,
                             storeId: cookie.storeId
-                        })
+                        });
                     });
-                    storage_cookies[name] = new_cookies;
 
+                    storage_cookies[name] = cookie_obj;
+                    setHighlight(storage_cookies, name);
                     let obj = {};
                     obj[domain] = storage_cookies;
                     chrome.storage.local.set(obj, function () {
-                        resolve();
+                        resolve(storage_cookies);
+
+                        if (typeof callback === 'function') {
+                            callback();
+                        } else {
+                            $('#input').val('');
+                            alertSuccess('保存成功!')
+                        }
                     });
                 });
-            })
-        }).then(function () {
+            });
+        }).then(function (storage_cookies) {
             removeLine(name);
-            appendLine(name);
-            console.log('save success')
+            appendLine(name, storage_cookies[name].highlight);
+            console.log('save success');
         }, function () {
-            console.log('save error')
+            alertError('保存失败!');
+            console.log('save error');
         });
     }
 
+    /**
+     * popup页面加载当前域名保存的所有session名称
+     * @param {object} url
+     */
     function loadAll(url) {
         let domain = url.host;
         chrome.storage.local.get(domain, function (cookies_obj) {
             if (cookies_obj.hasOwnProperty(domain)) {
                 let cookies = cookies_obj[domain] || {};
                 for (let name in cookies) {
-                    appendLine(name);
+                    appendLine(name, cookies[name].highlight);
                 }
             }
-        })
+        });
     }
 
-    function appendLine(name) {
+    /**
+     * 添加一行
+     * @param {string} name session名称
+     * @param {boolean} highlight 是否高亮
+     */
+    function appendLine(name, highlight = false) {
         let $list = $('#list');
         let dd = '<dd data-name="' + name + '">'
             + '<span>' + name + '</span>'
@@ -124,29 +259,45 @@ $(function () {
             + '<a class="remove">remove</a>'
             + '</dd>';
         $list.prepend(dd);
+        if (highlight) {
+            highlightLine(name);
+        }
     }
 
+    /**
+     * 删除一行（html）
+     * @param {string} name
+     */
     function removeLine(name) {
-        $('dd[data-name=' + name + ']').remove();
+        $('dd[data-name="' + name + '"]').remove();
     }
 
+    /**
+     * 高亮某一行
+     * @param {string} name
+     */
+    function highlightLine(name) {
+        $('dd[data-name="' + name + '"]').css('backgroundColor', '#bbffbb');
+        $('dd[data-name!="' + name + '"]').css('backgroundColor', 'transparent');
+    }
+
+    /**
+     * 获取当前 tab 的 url 信息之后, 执行更新、保存等操作(通过回调)
+     * @param {function} callback
+     */
     function action(callback) {
         new Promise(function (resolve, reject) {
-            chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, function (tabs) {
+            chrome.tabs.query({ 'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT }, function (tabs) {
                 let tab = tabs[0];
                 if (tab) {
                     let url = new URL(tab.url);
-                    if (url) {
-                        resolve(url);
-                    } else {
-                        reject('no active tab');
-                    }
+                    url ? resolve(url) : reject('no active tab');
                 }
             });
         }).then(function (url) {
             callback(url);
         }, function (error) {
             console.log(error);
-        })
+        });
     }
 });
